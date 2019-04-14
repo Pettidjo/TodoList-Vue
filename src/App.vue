d1939a5d3670b5f614bb4f4c196612d8609389ee
Jonathan Herbault <jonathanherbault@gmail.com>
2019-04-14 12:52:26 +0200

    todolist app crash course end

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
-  name: 'app',
+  name: "aspp",
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  } 
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    padding: 7px 20px;
+    color: #FFF;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-completed':todo.completed}">
+    <input type="checkbox" v-on:change="markComplete" />
+    <p>{{todo.title}}</p>
+    <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TodoItem",
+  props:["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background: #F4F4F4;
+    padding: 10px;
+    border-bottom: 1px #CCC dotted;
+  }
+
+  .is-completed {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #FFF;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+
+  input[type="checkbox"] {
+    display: inline-block;
+  }
+</style>
